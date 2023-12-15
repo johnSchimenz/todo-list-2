d8cdedeacc511260a08da2b8d00bd6c15f806a46
John Schimenz <john.schimenz@gmail.com>
2023-12-15 17:54:31 -0600

    Starting ProjectFactory

---
+++ main.js
@@ -0,0 +1,15 @@
+// Storage location of projects
+let projectsArray = [];
+
+// Project factory
+const ProjectFactory = () => {
+    title = title;
+    description = description;
+    dueDate = dueDate;
+    priority = priority;
+    notes = notes;
+    checklist = checklist;
+}
+
+
+// ToDo factory
