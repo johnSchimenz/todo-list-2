eba4efbcdf83d795cc7f9451d0992e0470e8e742
John Schimenz <john.schimenz@gmail.com>
2023-12-17 14:51:16 -0600

    Added array to list the arguments for ToDoFactory

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const ProjectFactory = (name) => {
     return (name, toDos);
 }
 
+// ToDo arguments array
+const toDoArgumentsArray = ['title', 'description', 'dueDate', 'priority', 'notes', 'checklist'];
+
 // ToDo factory
 const ToDoFactory = (title, description, dueDate, priority, notes, checklist) => {
     title = title;
@@ -31,6 +34,7 @@ createNewProject.addEventListener('click', () => {
     const projectInput = document.createElement('input');
     projectInput.setAttribute('type', 'text');
     projectInput.setAttribute('id', 'projectTitle');
+    projectInput.setAttribute('name', 'projectTitle');
     projectInput.setAttribute('placeholder', 'Anonymous Project');
     selectBottomLeftContainer.appendChild(projectInput);
 
@@ -77,18 +81,46 @@ createNewToDoItem.addEventListener('click', () => {
     selectBottomRightContainer.appendChild(toDoItemFieldset);
 
 
-    // Create input boxes for ToDo Item stuff
-    const toDoItemInput = document.createElement('input');
-
-
     /*
-    // Create
-    projectInput.setAttribute('type', 'text');
-    projectInput.setAttribute('id', 'projectTitle');
-    projectInput.setAttribute('placeholder', 'Anonymous Project');
-    selectBottomRightContainer.appendChild(projectInput);
-
+    // Create label for title input box
+    const toDoItemTitleLabel = document.createElement('label');    
+    toDoItemTitleLabel.setAttribute('for', 'toDoItemTitle');
+    toDoItemTitleLabel.textContent = "Title";
+    toDoItemFieldset.appendChild(toDoItemTitleLabel);
     
+    // Create title input box
+    const toDoItemTitleInput = document.createElement('input');    
+    toDoItemTitleInput.setAttribute('type', 'text');
+    toDoItemTitleInput.setAttribute('id', 'toDoItemTitle');
+    toDoItemTitleInput.setAttribute('name', 'toDoItemTitle');
+    toDoItemFieldset.appendChild(toDoItemTitleInput);
+    
+    // Create label for description input box
+    const toDoItemDescriptionLabel = document.createElement('label');  
+    toDoItemDescriptionLabel.setAttribute('for', 'toDoItemDescription');
+    toDoItemDescriptionLabel.textContent = "Description";
+    toDoItemFieldset.appendChild(toDoItemDescriptionLabel);
+    
+    // Create description input box
+    const toDoItemDescriptionInput = document.createElement('input');
+    toDoItemDescriptionInput.setAttribute('type', 'text');
+    toDoItemDescriptionInput.setAttribute('id', 'toDoItemDescription');
+    toDoItemDescriptionInput.setAttribute('name', 'toDoItemDescription');
+    toDoItemFieldset.appendChild(toDoItemDescriptionInput);
+
+    // Create label for dueDate input box
+    const toDoItemDueDateLabel = document.createElement('label');  
+    toDoItemDueDateLabel.setAttribute('for', 'toDoItemDueDate');
+    toDoItemDueDateLabel.textContent = "Due Date";
+    toDoItemFieldset.appendChild(toDoItemDueDateLabel);
+    
+    // Create dueDate input box
+    const toDoItemDueDateInput = document.createElement('input');
+    toDoItemDueDateInput.setAttribute('type', 'text');
+    toDoItemDueDateInput.setAttribute('id', 'toDoItemDueDate');
+    toDoItemDueDateInput.setAttribute('name', 'toDoItemDueDate');
+    toDoItemFieldset.appendChild(toDoItemDescriptionInput);
+
     // Create Submit button so can confirm the project's name
     const submitButton = document.createElement('button');
     submitButton.setAttribute('type', 'submit');
